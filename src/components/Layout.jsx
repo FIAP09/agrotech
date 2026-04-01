022e8fc63c28f6dd25360fd96c48f4ac1b34806d
Ramonzor7 <ramonmartins.2005@gmail.com>
2026-04-01 17:48:32 -0300

    Refactor Layout component to include theme props
    Updated Layout component to accept theme and toggleTheme props and modified structure.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,12 +2,16 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar.jsx";
 import Footer from "./Footer.jsx";
 
-export default function Layout() {
+export default function Layout({ theme, toggleTheme }) {
   return (
-    <>
-      <Navbar />
-      <Outlet />
+    <div className="layout">
+      <Navbar theme={theme} toggleTheme={toggleTheme} />
+
+      <main className="content">
+        <Outlet />
+      </main>
+
       <Footer />
-    </>
+    </div>
   );
 }
